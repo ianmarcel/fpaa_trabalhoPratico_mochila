e90c68053b8c10b1e4beb2de30b5ca0ffaa5224c
ianmarcel <ianmarcelcampos@gmail.com>
2022-05-09 16:50:11 -0300

    Update MainTeste.java

--- codigo/MainTeste.java
+++ codigo/MainTeste.java
@@ -3,14 +3,17 @@ public class MainTeste {
 
     public static void main(String args[]) {
 
-        int capacidadeAEscolher = 10;
+        //int capacidadeAEscolher; ta sendo passado por j(ele fica aumentando) Mas é so trocar o nome
         int quantItems;
-        mochila m = new mochila(capacidadeAEscolher);
+       
         //int contadorTempo = 0;
-        int j = 1;
-        long millis;
+        int j = 5; //pra n ficar de 5 em 5  MAS se der erro VOLTA PRA 1(n acho q vai precisar)
+        long tempoExecucao;
         //----------------------------------------------------
         do { //so pra testar em 1s
+            System.out.println("criando uma nova mochila com capacidade :"+j+" e um novo vetor de :"+j+" itens");
+            // mochila m = new mochila(capacidadeAEscolher);
+            mochila m = new mochila(j);
             quantItems = j;
             ItemMochila[] item = new ItemMochila[quantItems];
 
@@ -18,8 +21,9 @@ public class MainTeste {
             for (int i = 0; i < item.length; i++) {
                 item[i] = new ItemMochila();
             }
-
-            item = UtilLs.geraVetor(quantItems, false, capacidadeAEscolher);
+            
+            //item = UtilLs.geraVetor(quantItems, false, capacidadeAEscolher);  ********
+            item = UtilLs.geraVetor(quantItems, false, j);
             int tam = item.length;// pra usar no quicksort
             // item=UtilLs.geraVetor(4, false, 10);
             // testando(pode apagar esse for)
@@ -29,6 +33,8 @@ public class MainTeste {
             // System.out.println("razao : " + item[i].getRazao());
             // }
 
+
+             tempoExecucao=System.currentTimeMillis(); //dps declara ele la em cima
              UtilLs.bolhaInvertido(item);
             //UtilLs.quicksort(item, 0, tam - 1); // tem q ser tam-1 se n n pega
 
@@ -41,10 +47,23 @@ public class MainTeste {
             // imprimir os intens que estao na mochila
             m.getListaDeItens().forEach(System.out::println);
 
-             millis = System.currentTimeMillis(); //n lembro se no lugar de millis é tempoFinal e se teria uma outra varivavel(desse jeito)chamada tempoInicial
-            System.out.println("tempo ="+millis); // prints a Unix timestamp in milliseconds
-            System.out.println("tempo em seg ="+millis / 1000); // prints the same Unix timestamp in seconds
-            j++;
-        }while (millis < 4000);
+            tempoExecucao=(System.currentTimeMillis()-tempoExecucao);//n lembro se no lugar de millis é tempoFinal e se teria uma outra varivavel(desse jeito)chamada tempoInicial
+            if(tempoExecucao>100){
+                System.out.println("ha ha hateste ");
+            }
+            
+            System.out.println("tempo em milisegundos ="+tempoExecucao); // prints a Unix timestamp in milliseconds
+            //System.out.println("tempo em seg ="+tempoExecucao / 1000); // prints the same Unix timestamp in seconds
+            j=j+100; //pode ao invez de fazer os itens crescenrem de 1 em 1 pode ser de 5 em 5
+        
+        
+        
+        
+        
+             //ao invez de j ser pra qtdItens e capacidadeM faz um L(aumentando de 50 em 50) pra capacidade
+                //ai esse j seria a resposta da letra a
+            }while(j<26000); //Perto de 25000 ja leva 4s(com o bolha)
+        //while(j<100000);
+        //while (tempoExecucao < 800);
     }
 }
